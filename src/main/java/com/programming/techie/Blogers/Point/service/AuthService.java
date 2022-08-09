58e05ae1d0ec499e4debf3a50b379c1964e456bd
github_username <you@example.com>
2022-08-09 12:09:40 -0700

    first commit

---
+++ src/main/java/com/programming/techie/Blogers/Point/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.programming.techie.Blogers.Point.service;
+
+
+import com.programming.techie.Blogers.Point.dto.RegisterRequest;
+import com.programming.techie.Blogers.Point.model.User;
+import com.programming.techie.Blogers.Point.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+
+}
